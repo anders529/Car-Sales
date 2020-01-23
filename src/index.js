4e5fbaa470aec9976cfb9e9b8e9b43d4f47fb337
Aaron Andersen <anderseana@tutanota.com>
2020-01-23 17:19:53 -0600

    Site renders now, just need to find out why this stuff isn't adding or removing things.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import {ReducerThingy} from './reducers/ReducerThingy.js';
+
 import 'bulma/css/bulma.css';
 import './styles.scss';
+const store = createStore(ReducerThingy);
 const rootElement = document.getElementById('root');
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, rootElement);
+ReactDOM.render(<Provider store={store}><App /></Provider>, rootElement);
